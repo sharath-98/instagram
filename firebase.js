fbbc179bda44b43d71dd70a457223e46966c436c
sharath-98 <sharathchander.p@gmail.com>
2022-06-05 13:30:14 -0500

    Initial app setup

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase"
+const firebaseConfig = {
+  apiKey: "AIzaSyCWWY-S5L85oblVsoHzn_R6Ev3RkzCQrgM",
+  authDomain: "instagram-97a34.firebaseapp.com",
+  projectId: "instagram-97a34",
+  storageBucket: "instagram-97a34.appspot.com",
+  messagingSenderId: "25889209169",
+  appId: "1:25889209169:web:f3cc36dab13a994fdab170",
+  measurementId: "G-RX9D7R35C5"
+};
